d328a4a221708f059b0bb4716886adcd7d11775b
Abdul-Kadir Coskun <Abdulcoskun@hotmail.com>
2022-05-24 19:52:24 +1000

    Added TTS, Added safety returns to stop Audio spam.

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -3,23 +3,39 @@ import styles from "./Main.module.scss";
 import { Translator } from "./../../js/Translator";
 import { MorseAudio } from "./../../js/MorseAudio";
 
+const synth = window.speechSynthesis;
+const voices = synth.getVoices();
+
 export function Main() {
     const [input, setInput] = useState("");
     const [output, setOutput] = useState("");
     const [isEnglish, setState] = useState(true);
+    const [isPlaying, setIsPlaying] = useState(false);
 
-    function handleInput(e) {
-        setInput(e.target.value);
-    }
+    const handleInput = (e) => setInput(e.target.value);
 
     function handlePlay() {
-        if (input.length > 0) {
-            const audioCtx = new AudioContext();
-            const morseAudio = new MorseAudio(audioCtx);
-            morseAudio.connect(audioCtx.destination);
-            morseAudio.playString(0, isEnglish ? input : output);
+        if ((isPlaying && isEnglish) || synth.speaking || input.length < 0)
+            return;
+        setIsPlaying(true);
+        if (isEnglish) {
+            const morseAudio = new MorseAudio();
+            morseAudio.playString(0, input);
+        } else {
+            const speakText = new SpeechSynthesisUtterance(output);
+            speakText.voice = voices[0];
+            speakText.rate = 1;
+            speakText.pitch = 1;
+            synth.speak(speakText);
         }
+        setTimeout(
+            () => {
+                setIsPlaying(false);
+            },
+            isEnglish ? input.length * 1000 : output.length * 500
+        );
     }
+
     useEffect(() => {
         if (input.length > 0)
             if (input.match(/(^[a-zA-Z0-9])/)) setState(true);
